cb878e807bb8a8ef4bff1810d051ce07720f5103
Alessandro Arbasino <alessandroarbasino99@gmailcom>
2023-12-08 11:38:05 +0100

    navigation bar temp

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function showNavigationBar() {
-    let temp = document.getElementById("NavigationBar");
+    let temp = document.getElementById("NavigationBarTemp");
     let clon = temp.content.cloneNode(true);
     document.body.appendChild(clon);
 }
