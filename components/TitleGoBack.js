2f57b9c4128b9619ae16dd2ef45e4ad79b5168ae
alessandro arbasino <alessandroarbasino99@gmail.com>
2024-03-23 16:08:05 +0100

    centering some text

--- components/TitleGoBack.js
+++ components/TitleGoBack.js
@@ -4,11 +4,11 @@ import GoBackButton from '../components/GoBackButton';
 
 const TitleGoBack = ({Title}) => {
   return (
-    <View className=" mt-3 flex-row flex-1">
+    <View className=" flex-1 flex-row ">
       <View>
         <GoBackButton key={"Goback"}></GoBackButton>
       </View>
-      <View className=" flex-1">
+      <View className=" flex-1 justify-center items-center">
         <Text style={{textAlign: "center"}} className="font-semibold text-xl text-white">{Title}</Text>
       </View>
   </View>
