a5996fcdcace0227998fc4fc976dac543917f189
alessandro arbasino <alessandroarbasino99@gmail.com>
2024-04-03 14:58:40 +0200

    index with all images added

--- assets/index.js
+++ assets/index.js
@@ -23,3 +23,43 @@ export {default as RenderingRevengeLogo} from "./Icons/RenderingRevengeIcon.png"
 export {default as LostPlanetMazeLogo} from "./Icons/LostPlanetMazeIcon.png";
 export {default as FlappyBirdLikeLogo} from "./Icons/RenderingRevengeIcon.png";
 export {default as MYLastNeuronLogo} from "./Icons/MyLastNeuron.png";
+
+export {default as ChiselViolin} from "./Unity/ConventioLutheri/ChiselViolin.png";
+export {default as DrawRope} from "./Unity/ConventioLutheri/DrawRope.png";
+export {default as Interaction} from "./Unity/ConventioLutheri/Interaction.png";
+export {default as InteractionBasedStoryLine} from "./Unity/ConventioLutheri/InteractionBasedStoryLine.png";
+export {default as Malus} from "./Unity/ConventioLutheri/Malus.png";
+export {default as RopeAdded} from "./Unity/ConventioLutheri/RopeAdded.png";
+export {default as RopeTaken} from "./Unity/ConventioLutheri/RopeTaken.png";
+export {default as SandPaper} from "./Unity/ConventioLutheri/SandPaper.png";
+export {default as TakeViolinNeck} from "./Unity/ConventioLutheri/TakeViolinNeck.png";
+export {default as ViolinBuilt} from "./Unity/ConventioLutheri/ViolinBuilt.png";
+export {default as ViolinPainted} from "./Unity/ConventioLutheri/ViolinPainted.png";
+
+export {default as EclipseExodus} from "./Unity/EclipseExodus/EclipseExodus.png";
+export {default as Overlay} from "./Unity/EclipseExodus/Overlay.png";
+export {default as SunMode} from "./Unity/EclipseExodus/SunMode.png";
+export {default as Take} from "./Unity/EclipseExodus/Take.png";
+
+export {default as GameScene} from "./Unity/FlappyBirdLike/GameScene.png";
+export {default as GameSimulator} from "./Unity/FlappyBirdLike/GameSimulator.png";
+export {default as Menu} from "./Unity/FlappyBirdLike/Menu.png";
+export {default as Standings} from "./Unity/FlappyBirdLike/Standings.png";
+
+export {default as CustomizeCharacterMain} from "./Unity/LetItSlide/CustomizeCharacterMain.png";
+export {default as CustomizeHead} from "./Unity/LetItSlide/CustomizeHead.png";
+export {default as CustomizeShoes} from "./Unity/LetItSlide/CustomizeShoes.png";
+export {default as CustomizeSnowBoard} from "./Unity/LetItSlide/CustomizeSnowBoard.png";
+export {default as CustomizeSuit} from "./Unity/LetItSlide/CustomizeSuit.png";
+export {default as DrawLine} from "./Unity/LetItSlide/DrawLine.png";
+export {default as DynamicBoostMenu} from "./Unity/LetItSlide/DynamicBoostMenu.png";
+export {default as LetIiSlideHome} from "./Unity/LetItSlide/LetIiSlideHome.png";
+export {default as PatternPrevieuw} from "./Unity/LetItSlide/PatternPrevieuw.png";
+
+export {default as BeginningMatch} from "./Unity/LostPlanetMaze/BeginningMatch.png";
+export {default as CustomGridDimension} from "./Unity/LostPlanetMaze/CustomGridDimension.png";
+export {default as CustomGridDimension2} from "./Unity/LostPlanetMaze/CustomGridDimension2.png";
+export {default as JoinLobby} from "./Unity/LostPlanetMaze/JoinLobby.png";
+export {default as LobbyCreation} from "./Unity/LostPlanetMaze/LobbyCreation.png";
+export {default as MultiplayerGame} from "./Unity/LostPlanetMaze/MultiplayerGame.png";
+export {default as WaitingRoom} from "./Unity/LostPlanetMaze/WaitingRoom.png";
