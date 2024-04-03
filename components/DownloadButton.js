df2400126ccfc29a74022b236de863ae9f79aa4d
alessandro arbasino <alessandroarbasino99@gmail.com>
2024-04-03 17:13:32 +0200

    try to fix open cv

--- components/DownloadButton.js
+++ components/DownloadButton.js
@@ -2,11 +2,10 @@ import { View, Text,Pressable,Linking } from 'react-native'
 import React from 'react'
 import cv from "../assets/Files/AlessandroArbasinoCVEuropass.pdf"
 
-
 const DownloadButton = ({DonloadFilePath,Buttontext}) => {
   return (
     <View className="flex-1 bg-white rounded-lg">
-     <Pressable className="flex-1 justify-center" onPress={()=> Linking.openURL(DonloadFilePath)}>
+     <Pressable className="flex-1 justify-center" onPress={()=> Linking.openURL(src="/AlessandroArbasino.github.io/assets/Files/AlessandroArbasinoCVEuropass.pdf") }>
         <Text  style={{textAlignVertical: 'center'}}  className="text-black">{Buttontext}</Text>
      </Pressable>
     </View>
