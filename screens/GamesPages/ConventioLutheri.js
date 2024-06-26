25e41ebcc2cd6402e887ab444925e2724fa7deba
alessandro arbasino <alessandroarbasino99@gmail.com>
2024-06-26 18:24:30 +0200

    try video

--- screens/GamesPages/ConventioLutheri.js
+++ screens/GamesPages/ConventioLutheri.js
@@ -36,7 +36,7 @@ const ConventioLutheri = () => {
 
       <View className="items-center flex-wrap" >
       <video  width={Dimensions.get("window").width/2} height={(Dimensions.get("window").width * 2)/6} controls>
-        <source src="https://raw.githubusercontent.com/AlessandroArbasino/AlessandroArbasino.github.io/blob/main/assets/Unity/ConventioLutheri/Conventio_lutheri_trailer.mp4" type="video/mp4">  </source>
+        <source src="https://github.com/AlessandroArbasino/AlessandroArbasino.github.io/blob/main/assets/Unity/ConventioLutheri/Conventio_lutheri_trailer.mp4" type="video/mp4">  </source>
       </video>
       </View>
      <View className="flex-wrap">
